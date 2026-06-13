5f97e78f32d0b8367af284afdcb2267e70a6c229
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2026-06-13 16:27:46 +0900

    Save inference result to specified output path
    Added output argument to specify result image path.

--- inference.py
+++ inference.py
@@ -5,11 +5,13 @@ def main():
     parser = ArgumentParser()
     parser.add_argument('--image', type=str, required=True, help='test image path')
     parser.add_argument('--weights', type=str, required=True, help='pretrained weights path')
+    parser.add_argument('--output', type=str, default='result.jpg', help='output image path')
     args = parser.parse_args()
 
     model = YOLO(args.weights)
     results = model(args.image)
-    results[0].show()
+    results[0].save(filename=args.output)
+    print(f'Saved result to {args.output}')
 
 if __name__ == '__main__':
     main()

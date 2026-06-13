7e14b2fb6a22fbe787d235aee4d8fa702e0e12f8
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2026-06-13 15:47:16 +0900

    Update download link for sample videos

--- tools/download_sample_videos.py
+++ tools/download_sample_videos.py
@@ -1,7 +1,7 @@
 import zipfile
 from gdown import download
 
-gdown_fname = download('https://drive.google.com/uc?id=1Lx3YnJ15B5KCRA026KJSbpSQpBF-wh7j', quiet = False)
+gdown_fname = download('https://drive.google.com/uc?id=11qDIdeFM9h1NMfIchwOnmd7saRtmk1QF', quiet = False)
 
 with zipfile.ZipFile(gdown_fname) as f:
     f.extractall(path='.')
